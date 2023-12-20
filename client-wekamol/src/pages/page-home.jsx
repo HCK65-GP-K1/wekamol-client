d58206ec02ef93231f164e05660f20d63e1752e4
Reza Valovi <rezavalovi98@gmail.com>
2023-12-20 13:07:24 +0700

    feat: UIHome

--- client-wekamol/src/pages/page-home.jsx
+++ client-wekamol/src/pages/page-home.jsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 export default function HomePage() {
   let navigate = useNavigate();
 
-  let token = localStorage.getItem("access_token");
-
   const handleLogout = () => {
     localStorage.removeItem("access_token");
     navigate("/login");
@@ -12,17 +10,27 @@ export default function HomePage() {
   const handleToProfile = () => {
     navigate("/my");
   };
+  const handleToGame = () => {
+    navigate("/game");
+  };
 
   return (
     <>
-      <h1>INI HOMEPAGE, ISINYA FORM UNTUK MASUK SESSION GAME????</h1>
-      <button className="btn btn-primary">PLAY</button>
-      <button className="btn btn-primary" onClick={handleToProfile}>
-        PROFILE
-      </button>
-      <button className="btn btn-error" onClick={handleLogout}>
-        LOGOUT
-      </button>
+      <div className="relative" style={{
+        backgroundImage: `url('https://i.pinimg.com/236x/1e/89/44/1e89447eea5bb7ff361c003efaec1a50.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+        <div className="flex flex-row items-center justify-center h-screen md-30">
+          <button className="btn mr-4" onClick={handleToGame}>PLAY GAME</button>
+          <button className="btn " onClick={handleToProfile}>
+            PROFILE USER
+          </button>
+          <button className="btn btn-error absolute top-0 right-0 mt-4 mr-4" onClick={handleLogout}>
+            LOGOUT
+          </button>
+        </div>
+      </div>
     </>
   );
 }
